5339199f472ee1860804c800d54d3191d135631e
Raphael C <polyster20@gmail.com>
2023-11-22 16:03:57 +0100

    add cors policy and helmet

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://beta.dynamx.fr',
+      'https://dynamx.fr',
+    ],
+  });
+
+  app.use(helmet());
+
   await app.listen(process.env.PORT || 2728);
 }
 bootstrap();
