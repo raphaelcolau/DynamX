6b408324e999dcab69c53c4929370a992be27cbc
Raphael C <polyster20@gmail.com>
2023-11-25 20:03:59 +0100

    fix legacy code warning

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -1,14 +1,18 @@
 import { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
 import { wrapper } from '../src/app/tools/redux/store';
-import { darktheme } from '../src/app/assets/theme/darktheme';
 
-function App({ Component, pageProps }: AppProps) {
+import { darktheme } from '../src/app/assets/theme/darktheme';
+import { Provider } from 'react-redux';
+
+export default function App({ Component, ...rest }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { pageProps } = props || {};
     return (
+      <Provider store={store}>
         <ThemeProvider theme={darktheme}>
           <Component {...pageProps} />
         </ThemeProvider>
+      </Provider>
     );
 }
-
-export default wrapper.withRedux(App);
