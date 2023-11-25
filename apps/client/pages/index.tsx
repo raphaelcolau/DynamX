b5449bd351e7fe6b57a8e37185de9b3bdb4c0390
Raphael C <polyster20@gmail.com>
2023-11-25 17:27:56 +0100

    Create index.tsx

---
+++ apps/client/pages/index.tsx
@@ -0,0 +1,11 @@
+import PageLayout from "../src/app/components/layout/pageLayout";
+
+export default function Home() {
+    return (
+        <PageLayout>
+            <div>
+                <h1>Home</h1>
+            </div>
+        </PageLayout>
+    )
+}
