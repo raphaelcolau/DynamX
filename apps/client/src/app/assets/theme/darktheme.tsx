da61351c24089ea92022633f205fed03beb0c3d0
Raphael C <polyster20@gmail.com>
2023-11-25 17:29:05 +0100

    Create darktheme.tsx

---
+++ apps/client/src/app/assets/theme/darktheme.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+export const darktheme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#E06E0B',
+        },
+        secondary: {
+            main: '#f48fb1',
+        },
+        divider: '#303030',
+    },
+});
