5d14880aaa63651bc004987dca8edc3e013da6bf
Raphael C <polyster20@gmail.com>
2023-11-26 13:22:51 +0100

    refactoring

--- apps/client/src/app/components/sidebar/sidebar.tsx
+++ apps/client/src/app/components/sidebar/sidebar.tsx
@@ -14,21 +14,6 @@ import Logo from '../logo/logo';
 import { CustomThemeOptions } from '../../assets/theme/darktheme';
 
 export default function Sidebar() {
-    const StyledDrawer = styled('div')(({ theme }: { theme: CustomThemeOptions}) => ({
-        backgroundColor: theme.palette?.background?.paper,
-        borderRight: `1px solid ${theme.palette?.divider}`,
-        position: 'fixed',
-        top: 0,
-        [theme.breakpoints.up('sm')]: {
-            width: theme.drawerWidth,
-            height: '100vh',
-            flexShrink: 0,
-        },
-        "& .MuiDrawer-paper": {
-            width: theme.drawerWidth,
-            boxSizing: 'border-box',
-        },
-    }));
 
     const SidebarItem = () => {
         const tabList = [
