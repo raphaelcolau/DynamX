53034df1163e59f10902603651374b0712451106
Raphael C <polyster20@gmail.com>
2023-11-29 21:39:43 +0100

    Update download.tsx

--- apps/client/pages/download.tsx
+++ apps/client/pages/download.tsx
@@ -3,7 +3,7 @@ import PageLayout from "../src/app/components/layout/pageLayout";
 export default function Packs() {
     return (
         <PageLayout>
-            <h1>Download</h1>
+            <p>Download</p>
         </PageLayout>
     )
 }
