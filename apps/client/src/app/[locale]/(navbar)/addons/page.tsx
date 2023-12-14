980eb0da3ae4a85fccc0f8739b1004c69617e8b4
Raphael C <polyster20@gmail.com>
2023-12-14 11:12:24 +0100

    Update page.tsx

--- apps/client/src/app/[locale]/(navbar)/addons/page.tsx
+++ apps/client/src/app/[locale]/(navbar)/addons/page.tsx
@@ -1,17 +1,4 @@
 // app/page.tsx is the UI for '/addons' route
-import type { GetStaticPaths } from 'next'
-
-export const getStaticPaths: GetStaticPaths = async () => {
-    return {
-        paths: [
-            { params: { locale: 'fr' } },
-            { params: { locale: 'en' } },
-            { params: { locale: 'de' } },
-        ],
-        fallback: false,
-    }
-}
-
 
 export default function Page() {
     return (
