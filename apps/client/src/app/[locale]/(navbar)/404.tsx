60fa00bc6c40ad6809671b3eeff1d5b5c082c0b8
Raphael C <polyster20@gmail.com>
2024-02-02 21:10:57 +0100

    Update doc

---
+++ apps/client/src/app/[locale]/(navbar)/404.tsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material';
+
+export default function Custom404() {
+    return (
+        <Box>
+            <h1>404</h1>
+            <p>Page not found</p>
+        </Box>
+    )
+}
--- apps/client/src/app/[locale]/(navbar)/documentation/page.tsx
+++ apps/client/src/app/[locale]/(navbar)/documentation/page.tsx
@@ -1,9 +1,22 @@
+'use client';
 import { Box } from "@mui/material";
+import { useLocale } from "next-intl";
+import { useEffect } from "react";
 
 export default function Page() {
+    let locale = useLocale();
+
+    if (locale === 'de') locale = 'en';
+
+    useEffect(() => {
+        window.location.replace(`https://wiki.dynamx.fr/${locale}`);
+    });
+
     return (
-        <Box>
-            <h1> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor. Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.  </h1>
+        <Box
+            onClick={() => window.location.href = `https://wiki.dynamx.fr/${locale}`}
+        >
+            <h1>Redirecting to {locale}</h1>
         </Box>
     )
 }
